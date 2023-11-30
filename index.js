a902421ed464da91d0c7ea519b7b57763f2e9aa5
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2023-11-30 23:59:08 +0600

    added deployment

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      // "http://localhost:5173",
+      "https://daily-pulse-newspaper.web.app",
+      "https://daily-pulse-newspaper.firebaseapp.com"
+    ],
     credentials: true,
   })
 );
 app.use(express.json());
 app.use(cookieParser());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ibuouo3.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +32,21 @@ const client = new MongoClient(uri, {
   },
 });
 
+// middlewares
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access!" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access!" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -38,8 +57,22 @@ async function run() {
     const publishers = client.db("newspaperDB").collection("publishers");
     const users = client.db("newspaperDB").collection("users");
 
+    // ------------------------------------custom middlewares-------------------------------------------
+
+    // verify admin after verify token
+    const verifyAdmin = async (req, res, next) => {
+      const userEmail = req?.decoded?.email;
+      const filter = { email: userEmail };
+      const user = await users.findOne(filter);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access!" });
+      }
+      next();
+    };
+
     // ------------------------------------auth/jwt api-------------------------------------------
-    app.post("/jwt", async (req, res) => {
+    app.post("/jwt", (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
@@ -48,12 +81,12 @@ async function run() {
         .cookie("token", token, {
           httpOnly: true,
           secure: true,
-          sameSite: false,
+          sameSite: "none",
         })
         .send({ success: true });
     });
 
-    app.post("/logout", async (req, res) => {
+    app.post("/logout", (req, res) => {
       const user = req.body;
       res
         .clearCookie("token", {
@@ -64,18 +97,55 @@ async function run() {
 
     // ------------------------------------users all api-------------------------------------------
     // get all user
-    app.get("/users", async(req, res)=>{
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await users.find().toArray();
       res.send(result);
-    })
+    });
+
+    // make admin
+    app.put("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedRole = req.body;
+      const doc = {
+        $set: {
+          role: updatedRole.role,
+        },
+      };
+      const result = await users.updateOne(filter, doc, options);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await users.deleteOne(filter);
+      res.send(result);
+    });
+
+    // get admin form all users
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const userEmail = req.params.email;
+      if (req?.decoded?.email != userEmail) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const filter = { email: userEmail };
+      const user = await users.findOne(filter);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
 
     // get a user by email
-    app.get("/user/:email", async(req, res)=>{
+    app.get("/user/:email", async (req, res) => {
       const userEmail = req.params.email;
-      const filter = { email : userEmail };
+      const filter = { email: userEmail };
       const result = await users.findOne(filter);
       res.send(result);
-    })
+    });
 
     // post users
     app.post("/users", async (req, res) => {
@@ -93,7 +163,7 @@ async function run() {
     // ------------------------------------articles all api-------------------------------------------
 
     // get all articles
-    app.get("/articles", async (req, res) => {
+    app.get("/articles", verifyToken, verifyAdmin, async (req, res) => {
       const result = await articles.find().toArray();
       res.send(result);
     });
@@ -121,7 +191,7 @@ async function run() {
       res.send(result);
     });
     // premium articles
-    app.get("/premiumArticles", async (req, res) => {
+    app.get("/premiumArticles", verifyToken, async (req, res) => {
       const query = {
         isPremium: true,
         status: "approved",
@@ -130,27 +200,30 @@ async function run() {
       res.send(result);
     });
     // articles by author email
-    app.get("/articleByAuthor/:email", async (req, res) => {
+    app.get("/articleByAuthor/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
+      if (req.decoded.email !== email) {
+        return res.status(403).send({ message: "forbidden access!" });
+      }
       const query = { authorEmail: email };
       const result = await articles.find(query).toArray();
       res.send(result);
     });
     // post a article
-    app.post("/articles", async (req, res) => {
+    app.post("/articles", verifyToken, async (req, res) => {
       const article = req.body;
       const result = await articles.insertOne(article);
       res.send(result);
     });
     // delete a article
-    app.delete("/articles/:id", async (req, res) => {
+    app.delete("/articles/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await articles.deleteOne(filter);
       res.send(result);
     });
     // update article status
-    app.put("/articles/:id", async (req, res) => {
+    app.put("/articles/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
@@ -164,7 +237,7 @@ async function run() {
       res.send(result);
     });
     // update article by id
-    app.put("/updateArticle/:id", async (req, res) => {
+    app.put("/updateArticle/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
@@ -191,7 +264,7 @@ async function run() {
       res.send(result);
     });
     // publisher by publisher name
-    app.get("/publisher/:publisher", async (req, res) => {
+    app.get("/publisher/:publisher", verifyToken, async (req, res) => {
       const publisherParams = req.params.publisher;
       const query = { publisher: publisherParams };
       const result = await publishers.findOne(query);
